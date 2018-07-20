151106d44f7b117cb9273617e907c8db098b7247
PrincessTina <kristina.orl-va@bk.ru>
2018-07-20 23:59:28 +0300

    add popup window for dateIntervalPicker

--- calendar/src/static/js/components/DateIntervalPickerHidden.jsx
+++ calendar/src/static/js/components/DateIntervalPickerHidden.jsx
@@ -1,19 +1,49 @@
 import React from "react";
 
+import DateIntervalPickerOpened from './DateIntervalPickerOpened.jsx';
+
 import '../../css/dateIntervalPickerHidden.less';
 import icon from '../../images/calendar.png';
 
 export default class DateIntervalPickerHidden extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {visibilityOfDateIntervalPickerOpened: 'dateIntervalPicker__popupWindow'};
+
+        this.changeVisibilityOfDateIntervalPickerOpened = this.changeVisibilityOfDateIntervalPickerOpened.bind(this);
+    }
+
+    changeVisibilityOfDateIntervalPickerOpened() {
+        let className = this.state.visibilityOfDateIntervalPickerOpened;
+
+        switch (className) {
+            case 'dateIntervalPicker__popupWindow':
+                className = 'dateIntervalPicker__popupWindow-visible';
+                break;
+            default:
+                className = 'dateIntervalPicker__popupWindow';
+        }
+
+        this.setState({visibilityOfDateIntervalPickerOpened: className});
+    };
+
     render() {
         return (
-            <div className={'dateIntervalPickerHidden'}>
-                <div className={'wrapper'}>
-                    <div className={'interval'}>
-                        04.07 - 11.07.18 <i>(10:13 - 21:07)</i>
+            <div className={'dateIntervalPicker'}>
+                <div className={'dateIntervalPickerHidden'} onClick={this.changeVisibilityOfDateIntervalPickerOpened}>
+                    <div className={'wrapper'}>
+                        <div className={'interval'}>
+                            04.07 - 11.07.18 <i>(10:13 - 21:07)</i>
+                        </div>
+                        <div className={'calendar-icon'}>
+                            <img src={icon}/>
+                        </div>
                     </div>
-                    <div className={'calendar-icon'}>
-                        <img src={icon}/>
-                    </div>
+                </div>
+
+                <div className={this.state.visibilityOfDateIntervalPickerOpened}>
+                    <DateIntervalPickerOpened/>
                 </div>
             </div>
         );
---
+++ calendar/src/static/js/components/DateIntervalPickerOpened.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import '../../css/dateIntervalPickerOpened.less';
+
+export default class DateIntervalPickerOpened extends React.Component {
+    render() {
+        return (
+            <div className={"dateIntervalPickerOpened"}>
+                “Lorem ipsum” generator
+            </div>
+        );
+    }
+}
